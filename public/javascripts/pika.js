263ef2d7551bc0439ce732ea844c9b38ee829e72
Pika <bloodangel555@gmx.de>
2013-04-30 00:08:14 +0200

    After Many Problems with server/client communication so i switched to ajax
    changes: many things changed between server/client communication

--- public/javascripts/pika.js
+++ public/javascripts/pika.js
@@ -1,10 +1,102 @@
+///////////////
+//Global Vars//
+///////////////
+
+var gUSERNAME = "Anonymous";
+var gACT_PROFIL = 0;
+
 $(document).ready(function() {
 
-setTimeout(function () { 
-	$('#myTab a').click(function (e) {
-		console.log("blaaha");
-
-		$(this).tab('show');
-		})
-	}, 1000);
+	BootAnim();
 });
+
+function BootAnim(){
+		$('#myTab a').click(function (e) {
+			console.log("blaaha");
+
+			$(this).tab('show');
+			})
+}
+
+
+//////////////
+//Ajax Calls//
+//////////////
+
+////POST
+//Login
+function ajxLog(){
+	var user = document.getElementById("logName").value;
+	var pass = document.getElementById("logPass").value;
+	$.ajax({
+		type: "POST",
+		data: {username: user, password: pass},
+		
+		success: function(data){ ajxLogHelp(data);}
+	});
+}
+
+
+////GET
+//GetProfil
+function ajxWall(){
+	$.get("asd",{
+		//data: {profil: gACT_PROFIL},
+		
+		
+		success: function(data){ajxWallHelper(data);}
+	});
+}
+//GetWall
+
+
+
+
+///////////////
+//Ajax Helper//
+///////////////
+
+////POST
+//LoginHelper
+function ajxLogHelp(data){
+	if(data[0]){
+		gUSERNAME = data[1];
+		console.log("logged In")
+		document.location = "/profil/username";
+	 }
+	else{
+		console.log("wrong Pw");
+	}
+}
+
+
+////GET
+//ProfilHelper
+function ajxWallHelper(data){
+	document.location.hash = "Wall";
+	
+}
+
+///////////////
+//Ajax Getter//
+///////////////
+
+function ajxGetter(){
+	base = "http://127.0.0.1:3000";
+	switch(window.location){
+		case base+"/"
+	}
+}
+
+function ajxLogGet(){
+	
+}
+
+
+
+function CreatePost(data){
+	var newPost = document.createElement("div");
+		newPost.className = "wPost";
+		newPost.innerHTML = data;
+	var upperDiv = document.getElementById("");
+}
--- routes/index.js
+++ routes/index.js
@@ -8,14 +8,15 @@ res.render('index', { title: 'Express' });
 
 exports.profil = function(req, res){
 
-res.render('profil', { title: 'Express' });
+res.send(req.body.username);
+res.render('profil', { title: 'Express', penis: 1 });
 
 //res.get(profilname)
 };
 
 
 exports.wall = function(req, res){
-res.render('Tabs/wall', { title: 'wall' });
+res.render('Tabs/wall', { title: 'wall', loggedvariable: 1 });
 };
 
 exports.search = function(req, res){
@@ -25,29 +26,37 @@ res.render('Tabs/search', { title: 'search' });
 
 exports.profiles = function(req, res){
 res.render('Tabs/profiles', { title: 'profiles' });
-
 };
 
 
 
 
 // handler for form submitted from homepage
-exports.home_post_handler = function(req, res) {
+exports.home_post_handler = function(req, res, data) {
 
 	// get vars out of form
 	var username = req.body.username;
 	var password = req.body.password;
-    // store the username as a session variable
-	//if(loginChecker(username, password)){
+    
+	var data = new Array();
+		data[0] = 0;
+		data[1] = username;
+	if(loginCheck(username, password)){
+		// store the username as a session variable
 		req.session.username = username;
 		req.session.password = password;
-		console.log("Logged");
+		console.log("Logged: "+req.body.username);
+		data[0] = 1;
+	}		
+	res.send(data);
 	
-	
-		
-	//}
-	//req.session.password = password;
-	//console.log(username+"  "+password);
+
+
     // redirect the user to homepage
-    res.redirect('/profil/'+username);
+    //res.redirect('/profil/'+username);
 };
+
+function loginCheck(name, pass){
+	if(name == "pika" && pass == "bla"){return true;}
+	else{ return false };
+}
